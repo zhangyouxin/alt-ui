0768d1d9f238a4130bb0042ca3634603d8bf9aa3
zhangyouxin <zhangyouxingg.126.com>
2020-07-09 10:41:26 +0800

    add gzip

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,29 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const appConfig = require('./src/app.config')
+
+// 是否为生产环境
+const isProduction = process.env.NODE_ENV !== 'development'
 
 /** @type import('@vue/cli-service').ProjectOptions */
 module.exports = {
   productionSourceMap: false,
+  configureWebpack: (config) => {
+    // 生产环境相关配置
+    if (isProduction) {
+      // gzip压缩
+      const productionGzipExtensions = ['html', 'js', 'css']
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+          threshold: 10240, // 只有大小大于该值的资源会被处理 10240
+          minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+          deleteOriginalAssets: false, // 删除原文件
+        })
+      )
+    }
+  },
   // https://github.com/neutrinojs/webpack-chain/tree/v4#getting-started
   chainWebpack(config) {
     // We provide the app's title in Webpack's name field, so that
@@ -26,7 +47,7 @@ module.exports = {
   },
   css: {
     // Enable CSS source maps.
-    sourceMap: true,
+    sourceMap: false,
   },
   // Configure Webpack's dev server.
   // https://cli.vuejs.org/guide/cli-service.html
